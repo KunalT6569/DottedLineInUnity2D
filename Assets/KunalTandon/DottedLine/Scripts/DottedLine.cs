522cd626316e10b59da552a139d46fa3d31e24b7
KunalT6569 <kunaltandon.kt@gmail.com>
2018-06-06 22:30:23 +0530

    code added for dotted line maker

---
+++ Assets/KunalTandon/DottedLine/Scripts/DottedLine.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DottedLine
+{
+    public class DottedLine : MonoBehaviour
+    {
+        // Inspector fields
+        public Sprite Dot;
+        [Range(0.01f, 1f)]
+        public float Size;
+        [Range(0.1f, 2f)]
+        public float Delta;
+
+        //Static Property with backing field
+        private static DottedLine instance;
+        public static DottedLine Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = FindObjectOfType<DottedLine>();
+                return instance;
+            }
+        }
+
+        //Utility fields
+        List<Vector2> positions = new List<Vector2>();
+        List<GameObject> dots = new List<GameObject>();
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (positions.Count > 0)
+            {
+                DestroyAllDots();
+                positions.Clear();
+            }
+
+        }
+
+        private void DestroyAllDots()
+        {
+            foreach (var dot in dots)
+            {
+                Destroy(dot);
+            }
+            dots.Clear();
+        }
+
+        GameObject GetOneDot()
+        {
+            var gameObject = new GameObject();
+            gameObject.transform.localScale = Vector3.one * Size;
+            gameObject.transform.parent = transform;
+
+            var sr = gameObject.AddComponent<SpriteRenderer>();
+            sr.sprite = Dot;
+            return gameObject;
+        }
+
+        public void DrawDottedLine(Vector2 start, Vector2 end)
+        {
+            DestroyAllDots();
+
+            Vector2 point = start;
+            Vector2 direction = (end - start).normalized;
+
+            while ((end - start).magnitude > (point - start).magnitude)
+            {
+                positions.Add(point);
+                point += (direction * Delta);
+            }
+
+            Render();
+        }
+
+        private void Render()
+        {
+            foreach (var position in positions)
+            {
+                var g = GetOneDot();
+                g.transform.position = position;
+                dots.Add(g);
+            }
+        }
+    }
+}
---
+++ Assets/KunalTandon/DottedLine/Scripts/DottedLineDemo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DottedLineDemo : MonoBehaviour
+{
+    [Header("Points for line 1")]
+    public Vector2 pointA;
+    public Vector2 pointB;
+    [Header("Points for line 2")]
+    public Vector2 pointX;
+    public Vector2 pointY;
+    // Update is called once per frame
+    void Update()
+    {
+        //Creating a dotted line
+        DottedLine.DottedLine.Instance.DrawDottedLine(pointA, pointB);
+
+        //Creating another dotted line
+        DottedLine.DottedLine.Instance.DrawDottedLine(pointX, pointY);
+    }
+}
